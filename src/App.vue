ad61d9596ce2776e55780b5267750a0712041f28
arttuhar <arttuhar@gmail.com>
2022-01-19 16:04:35 +0200

    Empty project skeleton, ready for development

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import StartScreen from './components/StartScreen.vue';
+import QuestionScreen from './components/QuestionScreen.vue';
+import ResultsScreen from './components/ResultsScreen.vue';
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
